75e654c53eeb9066b98bf68bde02329645ab7295
Neeraj Singh <singh.neer@ymail.com>
2025-01-07 10:46:28 +0530

    components created

--- royal-rasoyi/src/app/app.component.ts
+++ royal-rasoyi/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopMenuHeaderComponent } from './components/shared/top-menu-header/top-menu-header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopMenuHeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'royal-rasoyi';
+  title = 'Royal रसोई | ';
 }
---
+++ royal-rasoyi/src/app/components/shared/top-menu-header/top-menu-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-menu-header',
+  imports: [],
+  templateUrl: './top-menu-header.component.html',
+  styleUrl: './top-menu-header.component.scss'
+})
+export class TopMenuHeaderComponent {
+
+}
