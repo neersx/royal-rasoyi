a8ba05ab46ce0037822f5533601ba78f68b98901
Neeraj Singh <singh.neer@ymail.com>
2025-01-08 22:38:16 +0530

    Food details page images fixed

--- royal-rasoyi/src/app/pages/food-details/food-details.component.ts
+++ royal-rasoyi/src/app/pages/food-details/food-details.component.ts
@@ -1,18 +1,20 @@
 import { Component, inject, Input } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { Router, ActivatedRoute, ParamMap, RouterModule } from '@angular/router';
+import { CommonModule, NgIf } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FoodMenuService } from '../../services/food-menu.service';
 
 @Component({
   selector: 'app-food-details',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
   standalone: true,
   templateUrl: './food-details.component.html',
   styleUrl: './food-details.component.scss',
 })
 export class FoodDetailsComponent {
   foodName: string | null = '';
-  foodDetails = {};
+  foodMenuList = [];
+  foodDetails : any;
 
   constructor(
     private readonly route: ActivatedRoute,
@@ -21,8 +23,46 @@ export class FoodDetailsComponent {
 
   ngOnInit(): void {
     this.foodName = this.route.snapshot.paramMap.get('name'); 
-    this.foodDetails = this.service
-      .getFoodMenu()
-      .filter((f: any) => f.name === this.foodName);
+    this.foodMenuList = this.service.getFoodMenu();
+    this.foodDetails = this.foodMenuList.filter((f: any) => f.name === this.foodName)[0];
+    this.foodDetails.relatedItems = this.getRelatedFoods(this.foodDetails.id)
+  }
+
+   /**
+   * Get related foods based on the same category.
+   * @param foodId - The ID of the current food item.
+   * @returns An array of related food items.
+   */
+   getRelatedFoods(foodId: number): Array<{ id: number; name: string; image: string; description: string }> {
+    // Find the category of the selected food item
+    const currentFood: any = this.foodMenuList.find((food: any) => food.id === foodId);
+
+    if (!currentFood) {
+      return []; // Return an empty array if the food item is not found
+    }
+
+    // Filter the food items by the same category but exclude the current food item
+    const relatedFoods = this.foodMenuList
+      .filter((food: any) => food.category === currentFood.category && food.id !== foodId)
+      .map((food: any) => ({
+        id: food.id,
+        name: food.name,
+        image: food.images[0],
+        description: food.description,
+      }));
+
+    return relatedFoods;
+  }
+
+  increaseQuantity(): any {
+    console.log('increased');
+  }
+
+  decreaseQuantity(): any {
+    console.log('increased');
+  }
+
+  addToCart(food: any): any {
+    console.log('add to cart', food);
   }
 }
