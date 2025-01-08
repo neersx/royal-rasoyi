5f3b46288c4b14e993dee9a4790db4e0b70bbb11
Neeraj Singh <singh.neer@ymail.com>
2025-01-08 23:43:43 +0530

    Basic meta and Title implemented in Food Details page

--- royal-rasoyi/src/app/pages/food-details/food-details.component.ts
+++ royal-rasoyi/src/app/pages/food-details/food-details.component.ts
@@ -1,8 +1,10 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, Inject, inject, Input, PLATFORM_ID } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap, RouterModule } from '@angular/router';
 import { CommonModule, NgIf } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FoodMenuService } from '../../services/food-menu.service';
+import { Meta, Title } from '@angular/platform-browser';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-food-details',
@@ -15,17 +17,27 @@ export class FoodDetailsComponent {
   foodName: string | null = '';
   foodMenuList = [];
   foodDetails : any;
+  isBrowser = true;
 
   constructor(
+    private readonly metaService: Meta,
+    private readonly titleService: Title,
+    private readonly router: Router,
     private readonly route: ActivatedRoute,
-    private readonly service: FoodMenuService
-  ) {}
+    private readonly service: FoodMenuService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    // Check if the platform is browser
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
 
   ngOnInit(): void {
     this.foodName = this.route.snapshot.paramMap.get('name'); 
     this.foodMenuList = this.service.getFoodMenu();
     this.foodDetails = this.foodMenuList.filter((f: any) => f.name === this.foodName)[0];
-    this.foodDetails.relatedItems = this.getRelatedFoods(this.foodDetails.id)
+    if(!this.foodDetails) return;
+    this.foodDetails.relatedItems = this.getRelatedFoods(this.foodDetails?.id);
+    this.updateMetaTags(this.foodDetails);
   }
 
    /**
@@ -46,12 +58,35 @@ export class FoodDetailsComponent {
       .filter((food: any) => food.category === currentFood.category && food.id !== foodId)
       .map((food: any) => ({
         id: food.id,
+        title: food.title,
         name: food.name,
         image: food.images[0],
         description: food.description,
       }));
 
     return relatedFoods;
+  }
+
+  updateMetaTags(product: any) {
+    this.titleService.setTitle(product.title);
+
+    let productPageUrl = '';
+
+    // Generate the product page URL only in the browser
+    if (this.isBrowser) {
+      productPageUrl = `${window.location.origin}${this.router.url}`;
+      this.metaService.updateTag({ property: 'og:url', content: productPageUrl });
+      this.metaService.updateTag({ property: 'twitter:url', content: productPageUrl });
+    }
+
+    this.metaService.updateTag({ property: 'twitter:title', content: product.title });
+    this.metaService.updateTag({ property: 'twitter:description', content: product.description });
+    this.metaService.updateTag({ property: 'twitter:image', content: product.images[0].url });
+
+    this.metaService.updateTag({ property: 'og:title', content: product.title });
+    this.metaService.updateTag({ property: 'og:description', content: product.description });
+    this.metaService.updateTag({ property: 'og:image', content: product.images[0].url });
+    
   }
 
   increaseQuantity(): any {
